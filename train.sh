2d19cd5d22b0de275a21bb938552f2ca7dea9738
Li Shen <shenli.sam@gmail.com>
2016-09-23 15:33:34 -0400

    Add echo for each step in train.sh

--- train.sh
+++ train.sh
@@ -3,12 +3,18 @@
 # Digital Mammography DREAM Challenge
 # Training inference method
 
+echo "==== nvcc version ===="
 nvcc --version
+echo "==== lspci command output ===="
 lspci | grep -i nvidia
+echo "==== nvidia-smi command output ===="
 nvidia-smi
+echo "==== python version ===="
 python -V
+echo "==== tensorflow package info ===="
 pip show tensorflow
 
 
 # Run training
+echo "==== Start training ===="
 python DREAM_DM_starter_tf.py --lr 0.0001 --decay 0.985 --bs 100 --epoch 11 --net GoogLe --ms 224 --dropout 0.6
