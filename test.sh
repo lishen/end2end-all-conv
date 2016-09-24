806e1a05f07703dc96c28b813b01b649ef1b90cd
Darvin <yidarvin@gmail.com>
2016-09-23 19:40:42 -0700

    fixed test script flag issue for super realsies

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 # Testing inference method
 
 # Run testing
-python DREAM_DM_starter_tf.py --net Le --ms 32 --test 1 -pf /scoringData
+python DREAM_DM_starter_tf.py --net Le --ms 32 --test 1 --pf /scoringData
