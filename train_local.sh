49ba36693cd1161fa8f5f28d5b872f5cb6d2e88f
Li Shen <shenli.sam@gmail.com>
2016-09-26 14:57:34 -0400

    Add a local training shell script

---
+++ train_local.sh
@@ -0,0 +1,14 @@
+python DREAM_DM_starter_tf.py \
+	--lr 0.0001 \
+	--decay 0.985 \
+	--bs 100 \
+	--epoch 11 \
+	--net GoogLe \
+	--ms 224 \
+	--dropout 0.6 \
+	--pf ./trainingData \
+	--csv1 ./metadata/images_crosswalk.tsv \
+	--csv2 ./metadata/exams_metadata.tsv \
+	--out ./modelState/out_train.txt \
+	--outtxt ./output/out.txt \
+	--saver ./modelState/model.ckpt
