99ac5e5e2a327b4d27f067cab941cb6f2fa13c4f
Li Shen <shenli.sam@gmail.com>
2016-09-26 16:36:21 -0400

    Modify train_local.sh

--- train_local.sh
+++ train_local.sh
@@ -2,7 +2,7 @@ python DREAM_DM_starter_tf.py \
 	--lr 0.0001 \
 	--decay 0.985 \
 	--bs 100 \
-	--epoch 11 \
+	--epoch 1 \
 	--net GoogLe \
 	--ms 224 \
 	--dropout 0.6 \
@@ -12,3 +12,6 @@ python DREAM_DM_starter_tf.py \
 	--out ./modelState/out_train.txt \
 	--outtxt ./output/out.txt \
 	--saver ./modelState/model.ckpt
+
+# One liner.
+# python DREAM_DM_starter_tf.py --lr 0.0001 --decay 0.985 --bs 100 --epoch 1 --net GoogLe --ms 224 --dropout 0.6 --pf ./trainingData --csv1 ./metadata/images_crosswalk.tsv --csv2 ./metadata/exams_metadata.tsv --out ./modelState/out_train.txt --outtxt ./output/out.txt --saver ./modelState/model.ckpt
