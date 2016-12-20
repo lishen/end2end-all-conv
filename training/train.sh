f015a58ddde3670e4d5b0685883b1f994ca98c26
Li Shen <shenli.sam@gmail.com>
2016-12-20 00:31:35 -0500

    Files re-organization between preprocessing and training

--- training/train.sh
+++ training/train.sh
@@ -1,16 +1,1 @@
 #!/bin/bash
-#
-# Digital Mammography DREAM Challenge
-# Training inference method
-
-echo "==== nvidia-smi command output ===="
-nvidia-smi
-echo "==== python version ===="
-python -V
-echo "==== tensorflow package info ===="
-pip show tensorflow
-
-
-# Run training
-echo "==== Start training ===="
-python DREAM_DM_starter_tf.py --lr 0.0001 --reg 0.0001 --decay 0.985 --bs 50 --time 240 --net GoogLe --ms 224 --dropout 0.6
