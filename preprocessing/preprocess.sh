7ae6b19ff41e1df9a86d2c654d9c8d3cc5e84f1a
Li Shen <shenli.sam@gmail.com>
2016-12-19 23:40:27 -0500

    Reduce message size in preprocessing scripts

--- preprocessing/preprocess.sh
+++ preprocessing/preprocess.sh
@@ -10,14 +10,16 @@ mkdir -p $CONVERTED_PNG_DIR $PROCESSED_PNG_DIR
 echo
 
 echo ">>> Convert .dcm files to .png files"
-find $TRAIN_DATA_DIR/ -maxdepth 1 -name '*.dcm' | parallel "echo 'convert: {} => $CONVERTED_PNG_DIR/{/.}.png'; convert {} $CONVERTED_PNG_DIR/{/.}.png"
-# find $CONVERTED_PNG_DIR -name '*.png' -execdir ls -lh {} \;
-echo "Done."
+# find $TRAIN_DATA_DIR/ -maxdepth 1 -name '*.dcm' | parallel "echo 'convert: {} => $CONVERTED_PNG_DIR/{/.}.png'; convert {} $CONVERTED_PNG_DIR/{/.}.png"
+find $TRAIN_DATA_DIR/ -maxdepth 1 -name '*.dcm' | parallel "convert {} $CONVERTED_PNG_DIR/{/.}.png"
+echo "Done converted $(find $CONVERTED_PNG_DIR/ -name '*.png'|wc -l) dcm files to png files."
 echo
 
 echo ">>> Process .png files using the image preprocessor"
-awk 'NR > 1 && $4 == "MLO" {print $6}' $IMG_TSV | parallel "echo 'process MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py --remove-pectoral $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
-awk 'NR > 1 && $4 != "MLO" {print $6}' $IMG_TSV | parallel "echo 'process non-MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+# awk 'NR > 1 && $4 == "MLO" {print $6}' $IMG_TSV | parallel "echo 'process MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py --remove-pectoral $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+awk 'NR > 1 && $4 == "MLO" {print $6}' $IMG_TSV | parallel "python preprocess.py --remove-pectoral $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+# awk 'NR > 1 && $4 != "MLO" {print $6}' $IMG_TSV | parallel "echo 'process non-MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+awk 'NR > 1 && $4 != "MLO" {print $6}' $IMG_TSV | parallel "python preprocess.py $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
 # find $PROCESSED_PNG_DIR -name '*.png' -execdir ls -lh {} \;
-echo "Done."
+echo "Done processed $(find $PROCESSED_PNG_DIR/ -name '*.png'|wc -l) png files."
 echo
--- preprocessing/preprocess_local.sh
+++ preprocessing/preprocess_local.sh
@@ -5,19 +5,21 @@ CONVERTED_PNG_DIR=./preprocessedData/png_org
 PROCESSED_PNG_DIR=./preprocessedData/png_prep
 IMG_TSV=./metadata/images_crosswalk.tsv
 
-echo ">>> Create folders for converted and processed .png files"
+echo "[$(date)] >>> Create folders for converted and processed .png files"
 mkdir -p $CONVERTED_PNG_DIR $PROCESSED_PNG_DIR
 echo
 
-echo ">>> Convert .dcm files to .png files"
-find $TRAIN_DATA_DIR/ -maxdepth 1 -name '*.dcm' | parallel "echo 'convert: {} => $CONVERTED_PNG_DIR/{/.}.png'; convert {} $CONVERTED_PNG_DIR/{/.}.png"
-# find $CONVERTED_PNG_DIR -name '*.png' -execdir ls -lh {} \;
-echo "Done."
+echo "[$(date)] >>> Convert .dcm files to .png files"
+# find $TRAIN_DATA_DIR/ -maxdepth 1 -name '*.dcm' | parallel "echo 'convert: {} => $CONVERTED_PNG_DIR/{/.}.png'; convert {} $CONVERTED_PNG_DIR/{/.}.png"
+find $TRAIN_DATA_DIR/ -maxdepth 1 -name '*.dcm' | parallel "convert {} $CONVERTED_PNG_DIR/{/.}.png"
+echo "[$(date)] Done converted $(find $CONVERTED_PNG_DIR/ -name '*.png'|wc -l) dcm files to png files."
 echo
 
-echo ">>> Process .png files using the image preprocessor"
-awk 'NR > 1 && $4 == "MLO" {print $6}' $IMG_TSV | parallel "echo 'process MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py --remove-pectoral $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
-awk 'NR > 1 && $4 != "MLO" {print $6}' $IMG_TSV | parallel "echo 'process non-MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+echo "[$(date)] >>> Process .png files using the image preprocessor"
+# awk 'NR > 1 && $4 == "MLO" {print $6}' $IMG_TSV | parallel "echo 'process MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py --remove-pectoral $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+awk 'NR > 1 && $4 == "MLO" {print $6}' $IMG_TSV | parallel "python preprocess.py --remove-pectoral $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+# awk 'NR > 1 && $4 != "MLO" {print $6}' $IMG_TSV | parallel "echo 'process non-MLO: $CONVERTED_PNG_DIR/{/.}.png => $PROCESSED_PNG_DIR/{/.}.png'; python preprocess.py $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
+awk 'NR > 1 && $4 != "MLO" {print $6}' $IMG_TSV | parallel "python preprocess.py $CONVERTED_PNG_DIR/{/.}.png $PROCESSED_PNG_DIR/{/.}.png"
 # find $PROCESSED_PNG_DIR -name '*.png' -execdir ls -lh {} \;
-echo "Done."
+echo "[$(date)] Done processed $(find $PROCESSED_PNG_DIR/ -name '*.png'|wc -l) png files."
 echo
