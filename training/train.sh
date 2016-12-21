d095e4364fb76eadd2be02edde3a0e8e06142a1a
Li Shen <shenli.sam@gmail.com>
2016-12-21 10:08:52 -0500

    Get scripts and docker files ready for training in cloud

--- training/train.sh
+++ training/train.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+export PYTHONPATH=/:$PYTHONPATH
+python dm_resnet_train.py --img-extension png \
+    --img-size 576 448 \
+    --val-size 0.1 \
+    --nb-worker 20 \
+    --exam-tsv /metadata/exams_metadata.tsv \
+    --img-tsv /metadata/images_crosswalk.tsv \
+    --net resnet50 \
+    --batch-size 32 \
+    --samples-per-epoch 32 \
+    --nb-epoch 100 \
+    --balance-classes .0 \
+    --weight-decay 0.0001 \
+    --lr-patience 5 \
+    --es-patience 10 \
+    --trained-model /modelState/dm_resnet50_lger_model.h5 \
+    /preprocessedData/png_576x448
+
--- training/train_local.sh
+++ training/train_local.sh
@@ -3,17 +3,18 @@
 # export PYTHONPATH=/:$PYTHONPATH
 python dm_resnet_train.py --img-extension png \
     --img-size 288 224 \
-    --val-size 0.1 \
+    --val-size 0.2 \
     --nb-worker 4 \
     --exam-tsv ./metadata/exams_metadata.tsv \
     --img-tsv ./metadata/images_crosswalk.tsv \
-    --net resnet50 \
-    --batch-size 32 \
-    --samples-per-epoch 64 \
-    --nb-epoch 5 \
+    --net resnet18 \
+    --batch-size 64 \
+    --samples-per-epoch 640 \
+    --nb-epoch 100 \
+    --balance-classes 4.0 \
     --weight-decay 0.0001 \
-    --lr-patience 5 \
-    --es-patience 10 \
+    --lr-patience 10 \
+    --es-patience 20 \
     --trained-model ./modelState/dm_resnet_model.h5 \
     ./preprocessedData/png_288x224
 
