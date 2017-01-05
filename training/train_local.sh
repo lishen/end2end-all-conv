c49a9d47e9874ceeec43681ac2a697f8c4d6d0c4
Li Shen <shenli.sam@gmail.com>
2017-01-05 12:57:53 -0500

    Update train_local.sh to reflect recent changes

--- training/train_local.sh
+++ training/train_local.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
 # export PYTHONPATH=/:$PYTHONPATH
-python dm_resnet_train.py --img-extension png \
-    --img-size 288 224 \
-    --val-size 0.2 \
-    --nb-worker 4 \
-    --exam-tsv ./metadata/exams_metadata.tsv \
-    --img-tsv ./metadata/images_crosswalk.tsv \
-    --net resnet18 \
-    --batch-size 64 \
-    --samples-per-epoch 640 \
-    --nb-epoch 100 \
-    --balance-classes 4.0 \
-    --weight-decay 0.0001 \
-    --lr-patience 10 \
-    --es-patience 20 \
-    --trained-model ./modelState/dm_resnet_model.h5 \
-    ./preprocessedData/png_288x224
+IMG_CW_TSV="./metadata/images_crosswalk.tsv"
+IMG_FOLDER="./preprocessedData/png_288x224"
+IMG_EXT="png"
 
+python dm_resnet_train.py \
+    --img-extension $IMG_EXT \
+    --img-size 288 224 \
+    --img-tsv $IMG_CW_TSV \
+    --multi-view \
+    --val-size 0.2 \
+    --featurewise-norm \
+    --batch-size 4 \
+    --samples-per-epoch 120 \
+    --nb-epoch 500 \
+    --balance-classes 1.0 \
+    --allneg-skip \
+    --net resnet18 \
+    --nb-init-filter 32 \
+    --init-filter-size 3 \
+    --init-conv-stride 2 \
+    --max-pooling-size 2 \
+    --max-pooling-stride 2 \
+    --weight-decay 0.0001 \
+    --alpha 0.0001 \
+    --l1-ratio 0.0 \
+    --inp-dropout 0.0 \
+    --hidden-dropout 0.0 \
+    --init-learningrate 0.01 \
+    --lr-patience 10 \
+    --es-patience 100 \
+    --best-model ./modelState/dm_resnet18_local_best_model.h5 \
+    --final-model ./modelState/dm_resnet18_local_final_model.h5 \
+    $IMG_FOLDER
+
+    # --exam-tsv ./metadata/exams_metadata.tsv \
