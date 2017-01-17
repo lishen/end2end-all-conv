dda8c34349fa587cb47f187c55218454f5ca4656
Li Shen <shenli.sam@gmail.com>
2017-01-17 10:54:16 -0500

    Modify the suffix for enet models to .pkl

--- training/train_enet.sh
+++ training/train_enet.sh
@@ -7,8 +7,8 @@ IMG_FOLDER="/trainingData"
 # IMG_FOLDER="./preprocessedData/png_288x224"
 IMG_EXT="dcm"
 # IMG_EXT="png"
-BEST_MODEL="/modelState/enet_288_bestAuc_model.h5"
-# FINAL_MODEL="./modelState/enet_288_local_final_model.h5"
+BEST_MODEL="/modelState/enet_288_bestAuc_model.pkl"
+# FINAL_MODEL="./modelState/enet_288_final_model.pkl"
 FINAL_MODEL="NOSAVE"
 # SAVED_MODEL="./modelState/2017-01-11_resnet50_288/resnet50_288_bestAuc_model.h5"
 DL_STATE="/resnet50_288_bestAuc_model_4.h5"
@@ -23,13 +23,13 @@ python dm_enet_train.py \
     --img-tsv $IMG_CW_TSV \
     --exam-tsv $EXAM_TSV \
     --multi-view \
-    --val-size 75 \
+    --val-size 7500 \
     --featurewise-norm \
     --featurewise-mean 485.9 \
     --featurewise-std 765.2 \
     --batch-size 32 \
-    --samples-per-epoch 640 \
-    --nb-epoch 20 \
+    --samples-per-epoch 64000 \
+    --nb-epoch 10 \
     --balance-classes 1.0 \
     --allneg-skip 1.0 \
     --pos-class-weight 1.0 \
@@ -37,7 +37,7 @@ python dm_enet_train.py \
     --l1-ratio 0.5 \
     --init-learningrate 0.1 \
     --lr-patience 2 \
-    --es-patience 10 \
+    --es-patience 4 \
     --dl-state $DL_STATE \
     --best-model $BEST_MODEL \
     --final-model $FINAL_MODEL \
