e5c470993c6725b3b8855f8fc2c6919ebd6beb01
Li Shen <shenli.sam@gmail.com>
2017-02-17 13:37:04 -0500

    Add more options in using DL clf for SC1 inference

--- inference/dm_sc1_candidROI_infer.py
+++ inference/dm_sc1_candidROI_infer.py
@@ -16,9 +16,11 @@ def run(img_folder, img_height=1024, img_scale=4095,
         roi_per_img=32, roi_size=(256, 256), 
         low_int_threshold=.05, blob_min_area=3, 
         blob_min_int=.5, blob_max_int=.85, blob_th_step=10,
+        roi_state=None, roi_bs=32,
         do_featurewise_norm=True, featurewise_mean=884.7, featurewise_std=745.3, 
         img_tsv='./metadata/images_crosswalk_prediction.tsv', exam_tsv=None,
         dl_state=None, dl_bs=32, nb_top_avg=1, validation_mode=False,
+        img_voting=False,
         out_pred='./output/predictions.tsv'):
     '''Run SC1 inference using the candidate ROI approach
     Notes: 
@@ -52,6 +54,20 @@ def run(img_folder, img_height=1024, img_scale=4095,
     else:
         class_mode = None
 
+    # Load ROI classifier.
+    if roi_state is not None:
+        roi_clf = load_model(
+            roi_state, 
+            custom_objects={
+                'sensitivity': DMMetrics.sensitivity, 
+                'specificity': DMMetrics.specificity
+            }
+        )
+        if gpu_count > 1:
+            roi_clf = make_parallel(roi_clf, gpu_count)
+    else:
+        roi_clf = None
+
     # Load model.
     if dl_state is not None:
         model = load_model(
@@ -75,11 +91,21 @@ def run(img_folder, img_height=1024, img_scale=4095,
             roi_size=roi_size,
             low_int_threshold=low_int_threshold, blob_min_area=blob_min_area,
             blob_min_int=blob_min_int, blob_max_int=blob_max_int, 
-            blob_th_step=blob_th_step, roi_clf=None, seed=random_seed)
-        roi_dat,_ = roi_generator.next()
+            blob_th_step=blob_th_step, 
+            roi_clf=roi_clf, clf_bs=roi_bs, return_sample_weight=True,
+            seed=random_seed)
+        roi_dat, roi_w = roi_generator.next()
         # import pdb; pdb.set_trace()
-        pred = model.predict(roi_dat, batch_size=dl_bs)
-        return pred.ravel()
+        pred = model.predict(roi_dat, batch_size=dl_bs).ravel()
+        if roi_clf is not None:
+            return np.average(pred, weights=roi_w)
+        elif img_voting:
+            pred = pred.reshape((-1, roi_per_img))
+            img_preds = [ np.sort(row)[-nb_top_avg:].mean() for row in pred ]
+            return np.mean(img_preds)
+        else:
+            return np.sort(pred)[-nb_top_avg:].mean()
+
 
     # Print header.
     fout = open(out_pred, 'w')
@@ -90,17 +116,13 @@ def run(img_folder, img_height=1024, img_scale=4095,
 
     for subj, exidx, exam in exam_list:
         try:
-            predL_arr = pred_img_list(exam['L']['img'])
+            predL = pred_img_list(exam['L']['img'])
         except KeyError:
-            predL_arr = np.array([.0])
+            predL = .0
         try:
-            predR_arr = pred_img_list(exam['R']['img'])
+            predR = pred_img_list(exam['R']['img'])
         except KeyError:
-            predR_arr = np.array([.0])
-        predL_arr.sort()
-        predR_arr.sort()
-        predL = predL_arr[-nb_top_avg:].mean()
-        predR = predR_arr[-nb_top_avg:].mean()
+            predR = .0
 
         try:
             cancerL = int(exam['L']['cancer'])
@@ -136,6 +158,9 @@ if __name__ == '__main__':
     parser.add_argument("--blob-min-int", dest="blob_min_int", type=float, default=.5)
     parser.add_argument("--blob-max-int", dest="blob_max_int", type=float, default=.85)
     parser.add_argument("--blob-th-step", dest="blob_th_step", type=int, default=10)
+    parser.add_argument("--roi-state", dest="roi_state", type=str, default=None)
+    parser.add_argument("--no-roi-state", dest="roi_state", action="store_const", const=None)
+    parser.add_argument("--roi-bs", dest="roi_bs", type=int, default=32)
     parser.add_argument("--featurewise-norm", dest="do_featurewise_norm", action="store_true")
     parser.add_argument("--no-featurewise-norm", dest="do_featurewise_norm", action="store_false")
     parser.set_defaults(do_featurewise_norm=True)
@@ -152,6 +177,9 @@ if __name__ == '__main__':
     parser.add_argument("--validation-mode", dest="validation_mode", action="store_true")
     parser.add_argument("--no-validation-mode", dest="validation_mode", action="store_false")
     parser.set_defaults(validation_mode=False)
+    parser.add_argument("--img-voting", dest="img_voting", action="store_true")
+    parser.add_argument("--no-img-voting", dest="img_voting", action="store_false")
+    parser.set_defaults(img_voting=False)
     parser.add_argument("--out-pred", "-o", dest="out_pred", type=str, 
                         default="./output/predictions.tsv")
 
@@ -166,6 +194,8 @@ if __name__ == '__main__':
         blob_min_int=args.blob_min_int,
         blob_max_int=args.blob_max_int,
         blob_th_step=args.blob_th_step,
+        roi_state=args.roi_state,
+        roi_bs=args.roi_bs,
         do_featurewise_norm=args.do_featurewise_norm,
         featurewise_mean=args.featurewise_mean,
         featurewise_std=args.featurewise_std,
@@ -175,6 +205,7 @@ if __name__ == '__main__':
         dl_bs=args.dl_bs,
         nb_top_avg=args.nb_top_avg,
         validation_mode=args.validation_mode,
+        img_voting=args.img_voting,
         out_pred=args.out_pred
     )
     print "\n>>> Inference options: <<<\n", run_opts, "\n"
