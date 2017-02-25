483b13a4ae03e7106618442480651492921ee78e
Li Shen <shenli.sam@gmail.com>
2017-02-24 22:09:33 -0500

    Obtain multiple outputs from a DL model

--- dm_enet.py
+++ dm_enet.py
@@ -25,12 +25,29 @@ class MultiViewDLElasticNet(object):
 
 
 class DLRepr(object):
-    '''Extract the last hidden layer representation for a DL model
+    '''Extract the hidden layer representations for a DL model
     '''
-    def __init__(self, dl_state, custom_objects=None):
+    def __init__(self, dl_state, custom_objects=None, 
+                 layer_name=None, layer_index=None):
+        '''DL representations for images
+        Args:
+            layer_name ([list]): names for the layers to extract. 
+            layer_index ([list]): indices for the layers to extract. index=-2
+                    corresponds to the last hidden layer. index=-4 
+                    corresponds to the last conv layer (before global 
+                    averaging).
+        '''
+        if layer_name is None and layer_index is None:
+            raise Exception("One of [layer_name, layer_index] must be specified")
         dl_model = load_model(dl_state, custom_objects=custom_objects)
-        self.repr_model = Model(input=dl_model.input, 
-                                output=dl_model.get_layer(index=-2).output)
+        if layer_index is not None:
+            output_list = [ dl_model.get_layer(index=idx).output 
+                            for idx in layer_index]
+        else:
+            output_list = [ dl_model.get_layer(name=nm).output 
+                            for nm in layer_name]
+
+        self.repr_model = Model(input=dl_model.input, output=output_list)
 
     def predict_on_batch(self, x):
         return self.repr_model.predict_on_batch(x)
@@ -43,7 +60,13 @@ class DLRepr(object):
                           nb_worker=1, pickle_safe=False):
         return self.repr_model.predict_generator(
             generator, val_samples, max_q_size=max_q_size,
-            nb_worker=nb_worker, pickle_safe=pickle_safe)        
+            nb_worker=nb_worker, pickle_safe=pickle_safe)
+
+    def get_output_shape(self):
+        return self.repr_model.output_shape
+
+    def dl_model_summary(self):
+        return self.repr_model.summary()
 
 
 
