31798c3f3e9f2ad3d92331c1c137d3d6c2bd40ff
Li Shen <shenli.sam@gmail.com>
2017-03-29 15:04:49 -0400

    Update heatmap score scripts for new pretrained DL models

--- training/dm_heatmap_score.py
+++ training/dm_heatmap_score.py
@@ -3,10 +3,10 @@ import os, sys
 import pickle
 import numpy as np
 from numpy.random import RandomState
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from keras.models import load_model
 from meta import DMMetaManager
-from dm_image import DMImageDataGenerator, add_img_margins, read_resize_img
+from dm_image import add_img_margins, read_resize_img
 from dm_keras_ext import DMMetrics as dmm
 from dm_multi_gpu import make_parallel
 from dm_preprocess import DMImagePreprocessor as prep
@@ -18,11 +18,12 @@ dim_ordering = K.image_dim_ordering()
 
 
 def get_prob_heatmap(img_list, target_height, target_scale, patch_size, stride, 
-                     model, batch_size,
-                     do_featurewise_norm=True, 
-                     featurewise_mean=873.6, featurewise_std=739.3):
+                     model, batch_size, preprocess):
     '''Sweep image data with a trained model to produce prob heatmaps
     '''
+    if img_list is None:
+        return None
+
     def sweep_img_patches(img):
         nb_row = int(float(img.shape[0] - patch_size)/stride)
         nb_col = int(float(img.shape[1] - patch_size)/stride)
@@ -34,10 +35,6 @@ def get_prob_heatmap(img_list, target_height, target_scale, patch_size, stride,
         for y in xrange(y_gap, y_gap + nb_row*stride, stride):
             for x in xrange(x_gap, x_gap + nb_col*stride, stride):
                 patch = img[y:y+patch_size, x:x+patch_size]
-                if do_featurewise_norm:
-                    patch = (patch - featurewise_mean)/featurewise_std
-                else:
-                    patch = (patch - patch.mean())/patch.std()
                 patch_list.append(patch)
         return np.stack(patch_list), nb_row, nb_col
 
@@ -50,29 +47,34 @@ def get_prob_heatmap(img_list, target_height, target_scale, patch_size, stride,
         img = add_img_margins(img, patch_size/2)
         patch_dat, nb_row, nb_col = sweep_img_patches(img)
         if dim_ordering == 'th':
-            patch_dat = patch_dat.reshape(
-                (patch_dat.shape[0], 1, 
-                 patch_dat.shape[1], 
-                 patch_dat.shape[2]))
+            patch_X = np.zeros((patch_dat.shape[0], 3, 
+                                patch_dat.shape[1], 
+                                patch_dat.shape[2]), 
+                                dtype='float64')
+            patch_X[:,0,:,:] = patch_dat
+            patch_X[:,1,:,:] = patch_dat
+            patch_X[:,2,:,:] = patch_dat
         else:
-            patch_dat = patch_dat.reshape(
-                (patch_dat.shape[0], 
-                 patch_dat.shape[1], 
-                 patch_dat.shape[2], 1))
-        pred = model.predict(patch_dat, batch_size=batch_size)
+            patch_X = np.zeros((patch_dat.shape[0], 
+                                patch_dat.shape[1], 
+                                patch_dat.shape[2], 3), 
+                                dtype='float64')
+            patch_X[:,:,:,0] = patch_dat
+            patch_X[:,:,:,1] = patch_dat
+            patch_X[:,:,:,2] = patch_dat
+        # import pdb; pdb.set_trace()
+        pred = model.predict(preprocess(patch_X), batch_size=batch_size)
         heatmap = pred.reshape((nb_row, nb_col, pred.shape[1]))
         heatmap_list.append(heatmap)
     return heatmap_list 
         
 
 def run(img_folder, dl_state, img_extension='dcm', 
-        img_height=1024, img_scale=4095, test_size=.2, neg_vs_pos_ratio=10., 
-        do_featurewise_norm=True, featurewise_mean=873.6, featurewise_std=739.3,
-        batch_size=128, patch_size=256, stride=8,
+        img_height=1024, img_scale=255., neg_vs_pos_ratio=1., 
+        net='vgg19', batch_size=128, patch_size=256, stride=8,
         exam_tsv='./metadata/exams_metadata.tsv',
         img_tsv='./metadata/images_crosswalk.tsv',
-        train_out='./modelState/prob_heatmap_train.pkl',
-        test_out='./modelState/prob_heatmap_test.pkl'):
+        out='./modelState/prob_heatmap.pkl'):
     '''Sweep mammograms with trained DL model to create prob heatmaps
     '''
     # Read some env variables.
@@ -86,20 +88,32 @@ def run(img_folder, dl_state, img_extension='dcm',
                              img_folder=img_folder, 
                              img_extension=img_extension)
     subj_list, subj_labs = meta_man.get_subj_labs()
-    subj_train, subj_test, labs_train, labs_test = train_test_split(
-        subj_list, subj_labs, test_size=test_size, stratify=subj_labs, 
-        random_state=random_seed)
+    subj_labs = np.array(subj_labs)
+    print "Found %d subjests" % (len(subj_list))
+    print "cancer patients=%d, normal patients=%d" \
+            % ((subj_labs==1).sum(), (subj_labs==0).sum())
+    # subj_train, subj_test, labs_train, labs_test = train_test_split(
+    #     subj_list, subj_labs, test_size=test_size, stratify=subj_labs, 
+    #     random_state=random_seed)
     if neg_vs_pos_ratio is not None:
-        subj_train, labs_train = DMMetaManager.subset_subj_list(
-            subj_train, labs_train, neg_vs_pos_ratio, random_seed)
+        subj_list, subj_labs = DMMetaManager.subset_subj_list(
+            subj_list, subj_labs, neg_vs_pos_ratio, random_seed)
+        subj_labs = np.array(subj_labs)
+        print "After subsetting, there are %d subjects" % (len(subj_list))
+        print "cancer patients=%d, normal patients=%d" \
+                % ((subj_labs==1).sum(), (subj_labs==0).sum())
 
-    # Get image and label lists.
+    # Get exam lists.
     # >>>> Debug <<<< #
-    subj_train = subj_train[:2]
-    subj_test = subj_test[:2]
+    # subj_list = subj_list[:2]
     # >>>> Debug <<<< #
-    img_train, ilab_train = meta_man.get_flatten_img_list(subj_train)
-    img_test, ilab_test = meta_man.get_flatten_img_list(subj_test)
+    print "Get flattened exam list"
+    exam_list = meta_man.get_flatten_exam_list(subj_list, cc_mlo_only=True)
+    exam_labs = meta_man.exam_labs(exam_list)
+    exam_labs = np.array(exam_labs)
+    print "positive exams=%d, negative exams=%d" \
+            % ((exam_labs==1).sum(), (exam_labs==0).sum())
+    sys.stdout.flush()
 
     # Load DL model.
     print "Load patch classifier:", dl_state; sys.stdout.flush()
@@ -116,47 +130,65 @@ def run(img_folder, dl_state, img_extension='dcm',
         sys.stdout.flush()
         dl_model = make_parallel(dl_model, gpu_count)
 
+    # Load preprocess function.
+    print "Load preprocess function for net:", net
+    if net == 'resnet50':
+        from keras.applications.resnet50 import ResNet50, preprocess_input
+    elif net == 'vgg16':
+        from keras.applications.vgg16 import VGG16, preprocess_input
+    elif net == 'vgg19':
+        from keras.applications.vgg19 import VGG19, preprocess_input
+    elif net == 'xception':
+        from keras.applications.xception import Xception, preprocess_input
+    elif net == 'inception':
+        from keras.applications.inception_v3 import InceptionV3, preprocess_input
+    else:
+        raise Exception("Pretrained model is not available: " + net)
+
     # Sweep the whole images and classify patches.
-    print "Generate prob heatmaps for train set.",
+    print "Generate prob heatmaps for exam list"
     sys.stdout.flush()
-    train_heatmaps = get_prob_heatmap(
-        img_train, img_height, img_scale, 
-        patch_size, stride, dl_model, batch_size, 
-        do_featurewise_norm, featurewise_mean, featurewise_std)
-    print "Done."
-    print "Generate prob heatmaps for test set.",
-    sys.stdout.flush()
-    test_heatmaps = get_prob_heatmap(
-        img_test, img_height, img_scale, 
-        patch_size, stride, dl_model, batch_size, 
-        do_featurewise_norm, featurewise_mean, featurewise_std)
+    heatmap_dat_list = []
+    for i,e in enumerate(exam_list):
+        dat = (e[0], e[1], 
+               {'L':{'cancer':e[2]['L']['cancer']}, 
+                'R':{'cancer':e[2]['R']['cancer']}})
+        dat[2]['L']['CC'] = get_prob_heatmap(
+            e[2]['L']['CC'], img_height, img_scale, patch_size, stride, 
+            dl_model, batch_size, preprocess_input)
+        dat[2]['L']['MLO'] = get_prob_heatmap(
+            e[2]['L']['MLO'], img_height, img_scale, patch_size, stride, 
+            dl_model, batch_size, preprocess_input)
+        dat[2]['R']['CC'] = get_prob_heatmap(
+            e[2]['R']['CC'], img_height, img_scale, patch_size, stride, 
+            dl_model, batch_size, preprocess_input)
+        dat[2]['R']['MLO'] = get_prob_heatmap(
+            e[2]['R']['MLO'], img_height, img_scale, patch_size, stride, 
+            dl_model, batch_size, preprocess_input)
+        heatmap_dat_list.append(dat)
+        print "processed %d/%d exams" % (i+1, len(exam_list))
+        sys.stdout.flush()
     print "Done."
 
     # Save the result.
     print "Saving result to external files.",
     sys.stdout.flush()
-    pickle.dump((train_heatmaps, img_train, ilab_train), open(train_out, 'w'))
-    pickle.dump((test_heatmaps, img_test, ilab_test), open(test_out, 'w'))
+    pickle.dump(heatmap_dat_list, open(out, 'w'))
     print "Done."
 
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="DM candidROI prediction")
+    parser = argparse.ArgumentParser(description="DM prob heatmap scoring")
     parser.add_argument("img_folder", type=str)
     parser.add_argument("dl_state", type=str)
     parser.add_argument("--img-extension", "-ext", dest="img_extension", type=str, default="dcm")
     parser.add_argument("--img-height", "-ih", dest="img_height", type=int, default=1024)
-    parser.add_argument("--img-scale", "-ic", dest="img_scale", type=int, default=4095)
-    parser.add_argument("--test-size", "-ts", dest="test_size", type=float, default=.2)
+    parser.add_argument("--img-scale", "-ic", dest="img_scale", type=float, default=255.)
     parser.add_argument("--neg-vs-pos-ratio", dest="neg_vs_pos_ratio", type=float, default=10.)
     parser.add_argument("--no-neg-vs-pos-ratio", dest="neg_vs_pos_ratio", 
                         action="store_const", const=None)
-    parser.add_argument("--featurewise-norm", dest="do_featurewise_norm", action="store_true")
-    parser.add_argument("--no-featurewise-norm", dest="do_featurewise_norm", action="store_false")
-    parser.set_defaults(do_featurewise_norm=True)
-    parser.add_argument("--featurewise-mean", dest="featurewise_mean", type=float, default=873.6)
-    parser.add_argument("--featurewise-std", dest="featurewise_std", type=float, default=739.3)
+    parser.add_argument("--net", dest="net", type=str, default="vgg19")    
     parser.add_argument("--batch-size", dest="batch_size", type=int, default=128)
     parser.add_argument("--patch-size", dest="patch_size", type=int, default=256)
     parser.add_argument("--stride", dest="stride", type=int, default=8)
@@ -165,28 +197,22 @@ if __name__ == '__main__':
     parser.add_argument("--no-exam-tsv", dest="exam_tsv", action="store_const", const=None)
     parser.add_argument("--img-tsv", "-it", dest="img_tsv", type=str, 
                         default="./metadata/images_crosswalk.tsv")
-    parser.add_argument("--train-out", dest="train_out", type=str, 
-                        default="./modelState/prob_heatmap_train.pkl")
-    parser.add_argument("--test-out", dest="test_out", type=str, 
-                        default="./modelState/prob_heatmap_test.pkl")
+    parser.add_argument("--out", dest="out", type=str, 
+                        default="./modelState/prob_heatmap.pkl")
 
     args = parser.parse_args()
     run_opts = dict(
         img_extension=args.img_extension, 
         img_height=args.img_height,
         img_scale=args.img_scale,
-        test_size=args.test_size if args.test_size < 1 else int(args.test_size), 
         neg_vs_pos_ratio=args.neg_vs_pos_ratio,
-        do_featurewise_norm=args.do_featurewise_norm,
-        featurewise_mean=args.featurewise_mean,
-        featurewise_std=args.featurewise_std,
+        net=args.net,
         batch_size=args.batch_size,
         patch_size=args.patch_size,
         stride=args.stride,
         exam_tsv=args.exam_tsv,
         img_tsv=args.img_tsv,
-        train_out=args.train_out,
-        test_out=args.test_out
+        out=args.out,
     )
     print "\n"
     print "img_folder=%s" % (args.img_folder)
--- training/score_heatmap_local.sh
+++ training/score_heatmap_local.sh
@@ -3,12 +3,10 @@
 # export PYTHONPATH=/:$PYTHONPATH
 IMG_CW_TSV="./metadata/images_crosswalk.tsv"
 EXAM_TSV="./metadata/exams_metadata.tsv"
-IMG_FOLDER="./trainingData"
-IMG_EXT="dcm"
-# DL_STATE="modelState/2017-03-16_candidROI_net50_mc5/resnet50_candidROI_mulcls_bestAuc_model5.h5"
-DL_STATE="modelState/2017-02-02_roi_256/roi_clf4.h5"
-TRAIN_OUT="./modelState/prob_heatmap_roiclf_s16_train.pkl"
-TEST_OUT="./modelState/prob_heatmap_roiclf_s16_test.pkl"
+IMG_FOLDER="preprocessedData/jpg_org"
+IMG_EXT="jpg"
+DL_STATE="modelState/ma_vs_ba_best_model2.009-0.93.h5"
+OUT="./modelState/prob_heatmap.pkl"
 
 echo -n "Start training: " && date
 echo
@@ -16,19 +14,15 @@ echo
 python dm_heatmap_score.py \
     --img-extension $IMG_EXT \
     --img-height 1024 \
-    --img-scale 4095 \
-    --test-size 0.3 \
-    --neg-vs-pos-ratio 10.0 \
-    --featurewise-norm \
-    --featurewise-mean 1111.6 \
-    --featurewise-std 718.1 \
+    --img-scale 255.0 \
+    --neg-vs-pos-ratio 1.0 \
+    --net vgg16 \
     --batch-size 32 \
-    --patch-size 256 \
-    --stride 16 \
+    --patch-size 96 \
+    --stride 96 \
     --img-tsv $IMG_CW_TSV \
     --exam-tsv $EXAM_TSV \
-    --train-out $TRAIN_OUT \
-    --test-out $TEST_OUT \
+    --out $OUT \
     $IMG_FOLDER $DL_STATE
 
 echo
