31e621259869683b4634ec077f090e3b95fa5026
Li Shen <shenli.sam@gmail.com>
2017-03-31 22:43:47 -0400

    Update score heatmap shell script

--- training/score_heatmap_local.sh
+++ training/score_heatmap_local.sh
@@ -3,10 +3,10 @@
 # export PYTHONPATH=/:$PYTHONPATH
 IMG_CW_TSV="./metadata/images_crosswalk.tsv"
 EXAM_TSV="./metadata/exams_metadata.tsv"
-IMG_FOLDER="preprocessedData/jpg_org"
-IMG_EXT="jpg"
-DL_STATE="modelState/ma_vs_ba_best_model2.009-0.93.h5"
-OUT="./modelState/prob_heatmap.pkl"
+IMG_FOLDER="trainingData"
+IMG_EXT="dcm"
+DL_STATE="modelState/2017-03-31_patch_im1024_96/all_cls_best_model3.h5"
+OUT="./modelState/2017-03-31_patch_im1024_96/all_cls_best_model3_phm_s6.pkl"
 
 echo -n "Start training: " && date
 echo
@@ -15,11 +15,11 @@ python dm_heatmap_score.py \
     --img-extension $IMG_EXT \
     --img-height 1024 \
     --img-scale 255.0 \
-    --neg-vs-pos-ratio 1.0 \
-    --net vgg16 \
-    --batch-size 32 \
+    --no-neg-vs-pos-ratio \
+    --net vgg19 \
+    --batch-size 200 \
     --patch-size 96 \
-    --stride 96 \
+    --stride 6 \
     --img-tsv $IMG_CW_TSV \
     --exam-tsv $EXAM_TSV \
     --out $OUT \
